93ae36c082236858d004f6d40df2d6c553f1ea1e
Michael Tolis <tolism.info@gmail.com>
2018-03-08 15:12:56 +0100

    Update learning process

---
+++ 04-Functions/ex1-5/cats.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Ma page web</title>
+    </head>
+    <body><h1>Page 2</h1>
+    	<img src="Souris.jpg">
+    	<a href="./index.php">lien</a>
+
+	</body>
+</html>
---
+++ 04-Functions/ex1-5/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Ma page web</title>
+    </head>
+    <body><h1>Bonjour <?php echo 'michael'?>!</h1>
+    		<img src="Souris.jpg">
+    		<a href="./cats.php">lien</a>
+</html>
